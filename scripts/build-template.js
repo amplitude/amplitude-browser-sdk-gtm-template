ad2739b7bccd53498acb49db41cdcb4a35351a72
Daniel Graham <daniel.graham@amplitude.com>
2025-09-23 15:40:34 -0700

    move template-info and parameters to separate json files

--- scripts/build-template.js
+++ scripts/build-template.js
@@ -1,15 +1,25 @@
 const fs = require('fs');
 const ejs = require('ejs');
 const sandboxedJs = fs.readFileSync('libs/sandboxed-js.js', 'utf8');
+const templateParametersJson = fs.readFileSync('libs/template-parameters.json', 'utf8');
+const infoJson = fs.readFileSync('libs/template-info.json', 'utf8');
 
 // assert that the variable sandboxedJs is in the template
 const template = fs.readFileSync('template.tpl.ejs', 'utf8');
 if (!template.includes('<%- sandboxedJs %>')) {
     throw new Error('sandboxedJs is not in the template');
 }
+if (!template.includes('<%- templateParametersJson %>')) {
+    throw new Error('templateParametersJson is not in the template');
+}
+if (!template.includes('<%- infoJson %>')) {
+    throw new Error('infoJson is not in the template');
+}
 
 const data = {
-    sandboxedJs: sandboxedJs
+    sandboxedJs: sandboxedJs,
+    templateParametersJson: templateParametersJson,
+    infoJson: infoJson,
 };
 const options = {
     filename: 'template.tpl.ejs',
