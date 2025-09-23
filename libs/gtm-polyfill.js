d8fd344a4e1a01cb29288369736d2ad127ae1d4a
Daniel Graham <daniel.graham@amplitude.com>
2025-09-23 15:31:23 -0700

    verify templates have proper variables

--- libs/gtm-polyfill.js
+++ libs/gtm-polyfill.js
@@ -19,14 +19,14 @@
     // Injects a script tag
     function injectScript(url, onSuccess, onFailure) {
         if (typeof document !== "undefined") {
-        const script = document.createElement("script");
-        script.src = url;
-        script.onload = () => onSuccess && onSuccess();
-        script.onerror = () => onFailure && onFailure();
-        document.head.appendChild(script);
+            const script = document.createElement("script");
+            script.src = url;
+            script.onload = () => onSuccess && onSuccess();
+            script.onerror = () => onFailure && onFailure();
+            document.head.appendChild(script);
         } else {
-        console.warn("injectScript called outside browser:", url);
-        if (onFailure) onFailure();
+            console.warn("injectScript called outside browser:", url);
+            if (onFailure) onFailure();
         }
     }
     
--- scripts/build-template.js
+++ scripts/build-template.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
 const ejs = require('ejs');
 const sandboxedJs = fs.readFileSync('libs/sandboxed-js.js', 'utf8');
+
+// assert that the variable sandboxedJs is in the template
+const template = fs.readFileSync('template.tpl.ejs', 'utf8');
+if (!template.includes('<%- sandboxedJs %>')) {
+    throw new Error('sandboxedJs is not in the template');
+}
+
 const data = {
     sandboxedJs: sandboxedJs
 };
 const options = {
-    filename: 'template.tpl.ejs'
+    filename: 'template.tpl.ejs',
 };
 
 ejs.renderFile('template.tpl.ejs', data, options, function(err, str){
