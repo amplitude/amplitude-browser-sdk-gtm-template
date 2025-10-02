9241e08e3ed81f27232acdb6a148444969a8aa6a
Daniel Graham <daniel.graham@amplitude.com>
2025-10-02 09:49:28 -0700

    upgrade tag version (#95)

--- libs/sandboxed-js.js
+++ libs/sandboxed-js.js
@@ -10,7 +10,7 @@ const makeTableMap = require('makeTableMap');
 const JSON = require('JSON');
 
 // Constants
-const WRAPPER_VERSION = '3.20.0';
+const WRAPPER_VERSION = '3.21.0';
 const JS_URL = 'https://cdn.amplitude.com/libs/analytics-browser-gtm-wrapper-'+WRAPPER_VERSION+'.js.br';
 const LOG_PREFIX = '[Amplitude / GTM] ';
 const WRAPPER_NAMESPACE = '_amplitude';
